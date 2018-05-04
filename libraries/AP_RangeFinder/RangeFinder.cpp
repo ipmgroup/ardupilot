0970cf7f1839336e4b45f6447c9fbd8e85fcf641
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2018-05-04 18:05:27 +0200

    Added address configuration check for RangeFinder_TYPE_VL53L0X.

--- libraries/AP_RangeFinder/RangeFinder.cpp
+++ libraries/AP_RangeFinder/RangeFinder.cpp
@@ -678,11 +678,13 @@ void RangeFinder::detect_instance(uint8_t instance, uint8_t& serial_instance)
         }
         break;
     case RangeFinder_TYPE_VL53L0X:
-        if (!_add_backend(AP_RangeFinder_VL53L0X::detect(state[instance],
-                                                         hal.i2c_mgr->get_device(1, 0x29)))) {
-            _add_backend(AP_RangeFinder_VL53L0X::detect(state[instance],
-                                                        hal.i2c_mgr->get_device(0, 0x29)));
-        }
+    	if (state[instance].address) {
+			if (!_add_backend(AP_RangeFinder_VL53L0X::detect(state[instance],
+															 hal.i2c_mgr->get_device(1, state[instance].address==0?0x29:state[instance].address)))) {
+				_add_backend(AP_RangeFinder_VL53L0X::detect(state[instance],
+															hal.i2c_mgr->get_device(0, state[instance].address==0?0x29:state[instance].address)));
+			}
+    	}
         break;
 #if CONFIG_HAL_BOARD == HAL_BOARD_PX4
     case RangeFinder_TYPE_PX4_PWM:
