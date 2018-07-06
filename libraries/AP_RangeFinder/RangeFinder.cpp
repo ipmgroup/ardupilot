0cc8d34158ccc4f7f7bb2644fdc7c60b8de64110
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2018-07-06 16:35:49 +0200

    RangeFinder: another fix related to px4-v4pro

--- libraries/AP_RangeFinder/RangeFinder.cpp
+++ libraries/AP_RangeFinder/RangeFinder.cpp
@@ -292,7 +292,7 @@ void RangeFinder::detect_instance(uint8_t instance, uint8_t& serial_instance)
 #if CONFIG_HAL_BOARD == HAL_BOARD_PX4
     case RangeFinder_TYPE_PX4_PWM:
         if (AP_RangeFinder_PX4_PWM::detect()) {
-            drivers[instance] = new AP_RangeFinder_PX4_PWM(state[instance], _powersave_range, estimated_terrain_height);
+            drivers[instance] = new AP_RangeFinder_PX4_PWM(state[instance], params[instance], _powersave_range, estimated_terrain_height);
         }
         break;
 #endif
